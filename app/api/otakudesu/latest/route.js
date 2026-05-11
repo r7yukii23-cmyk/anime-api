b85734e31a45ba4e06acf305a2bfe4e3d16aff1d
Yue Official <r7yukii23@gmail.com>
2026-05-11 11:05:18 +0700

    Update route.js

--- app/api/otakudesu/latest/route.js
+++ app/api/otakudesu/latest/route.js
@@ -2,24 +2,29 @@ import { getAllAnime } from "@/lib/engine"
 
 export async function GET() {
   try {
-    const data = await getAllAnime(1) // batasi biar aman
+    const data = await getAllAnime(1)
 
-    return Response.json({
-      status: "success",
-      statusCode: 200,
-      statusmessage: "OK",
-      creator: "YUE",
-      message: "",
-      count: data.length,
-      data
-    })
+    return Response.json(
+      {
+        status: "success",
+        statusCode: 200,
+        creator: "YUE",
+        message: "OK",
+        count: data.length,
+        data
+      },
+      { status: 200 }
+    )
   } catch (err) {
-    return Response.json({
-      status: "error",
-      statusCode: 500,
-      creator: "YUE",
-      message: err.message,
-      data: []
-    })
+    return Response.json(
+      {
+        status: "error",
+        statusCode: 500,
+        creator: "YUE",
+        message: err.message,
+        data: []
+      },
+      { status: 500 }
+    )
   }
 }
